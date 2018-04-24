c4ee3361e6f981e45d7ba1841b3425d3add71a5a
Alexandre <alexandre@heapway.com>
2018-04-24 14:58:10 +0200

    Initial commit

---
+++ app/src/main/java/org/diiage/martin/broadcastintentsmartin/MainActivity.java
@@ -0,0 +1,22 @@
+package org.diiage.martin.broadcastintentsmartin;
+
+import android.content.BroadcastReceiver;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BroadcastReceiver br = new MyBroadcastReceiver();
+        IntentFilter filter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        filter.addAction(Intent.ACTION_BATTERY_CHANGED);
+        this.registerReceiver(br, filter);
+    }
+}
